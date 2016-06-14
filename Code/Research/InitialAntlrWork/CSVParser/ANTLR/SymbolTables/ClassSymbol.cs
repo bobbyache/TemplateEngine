1f6dfd12858cb2657fbe0dd4fa1e8c825504ecf0
bobyache <robbie.a.blake@gmail.com>
2016-06-14 11:13:41 +0200

    Initial work trying to understand ANTLR.

---
+++ Code/Research/InitialAntlrWork/CSVParser/ANTLR/SymbolTables/ClassSymbol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVParsing.ANTLR.SymbolTables
+{
+    //public class ClassSymbol : Symbol, IType
+    //{
+    //    string IType.Name
+    //    {
+    //        get { throw new NotImplementedException(); }
+    //    }
+    //}
+}
---
+++ Code/Research/InitialAntlrWork/CSVParser/ANTLR/SymbolTables/IType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVParsing.ANTLR.SymbolTables
+{
+    public interface IType
+    {
+        string Name { get; }
+    }
+}
