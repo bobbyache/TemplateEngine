9eb5a876c6c132463a253b974d0d8840037d1f10
RobBl8ke <RobB@korbicom.com>
2021-02-08 13:03:02 +0200

    Removed the FunctionPlugins project. Its not being used.

--- Code/CygSoft.Qik.LanguageEngine.FunctionPlugins/IFuncInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CygSoft.Qik.LanguageEngine.FunctionPlugins
-{
-    public interface IFuncInfo
-    {
-        int Column { get; }
-        int Line { get; }
-        string Name { get; }
-    }
-}
